f8151beeed2796db17569699b3697901fcc0e053
Jason Chan <jchan321738@gmail.com>
2021-04-24 17:13:45 -0700

    Create forward, right, left, allPoints

--- turtle_graphics/turtle_graphics.js
+++ turtle_graphics/turtle_graphics.js
@@ -1,21 +1,69 @@
-class turtle {
+class Turtle {
     constructor(x,y) {
-        this.x = x;
-        this.y = y;
+        this.x = x;     // 0
+        this.y = y;     // 0
+        this.degree = 90;
+        this.coordinates = [];
     }
-    forward() {
 
+    forward(steps = 0) {
+        if (this.degree === 90) { // East
+            for (let i = 1; i <= steps; i++) {
+                this.coordinates.push([this.x, this.y]);
+                this.x++;
+            }
+            return this;
+        } else if (this.degree === 180) { // South
+            for (let i = 1; i <= steps; i++) {
+                this.coordinates.push([this.x, this.y])
+                this.y++;
+            }
+            return this;
+        } else if (this.degree === 270) { // West
+            for (let i = 1; i <= steps; i++) {
+                this.coordinates.push([this.x, this.y])
+                this.x--;
+            }
+            return this;
+        } else if (this.degree === 0 || this.degree == 360) { // North
+            for (let i = 1; i <= steps; i++) {
+                this.coordinates.push([this.x, this.y])
+                this.y--;
+            }
+            return this;
+        } else if (this.degree > 360) { // East 360+
+            this.degree = 90
+            for (let i = 1; i <= steps; i++) {
+                this.coordinates.push([this.x, this.y]);
+                this.x++;
+            }
+            return this;
+        } else if (this.degree < 0) { // West 0-
+            this.degree = 270
+            for (let i = 1; i <= steps; i++) {
+                this.coordinates.push([this.x, this.y])
+                this.x--;
+            }
+            return this;
+        }
     }
+
     right() {
-
+        this.degree  += 90
+        return this;
     }
+
     left() {
-
+        this.degree -= 90
+        return this;
     }
-    allPoint() {
 
+    allPoints() {
+        console.log(this.coordinates);
     }
-    print() {
+
+    print() { // ■ □
         
-    }
+    }   
 }
+
